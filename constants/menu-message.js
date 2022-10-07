52b820b5ef0d417159e97cb9df86bf05f5a34a79
Hermógenes Neto <61895005+Hermogenes00@users.noreply.github.com>
2022-10-06 23:35:40 -0300

    Update menu-message.js

--- constants/menu-message.js
+++ constants/menu-message.js
@@ -2,7 +2,7 @@ export default `
   /start = ativa o bot, para realizar o recebimento dos resultados da eleição a cada ${
     process.env.TIMER ? Number.parseInt(process.env.TIMER / 1000) : 10
   } segundos\n
-  /desativa = desativa o bot\n
+  /desativar = desativa o bot\n
   /resultados = obtém resultado das eleições\n
   /ajuda = exibe novamente este menu, para suporte.
   `;
